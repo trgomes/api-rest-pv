f18051671c304bab8335d4e3f5341746fd3037f9
trgomes <trtg10@gmail.com>
2017-12-02 03:36:55 -0200

    Inserindo busca - getAll e getById

--- app/Voo.php
+++ app/Voo.php
@@ -3,6 +3,7 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Facades\DB;
 
 class Voo extends Model
 {
@@ -17,6 +18,29 @@ class Voo extends Model
 
     public function aeroportoDestino(){
         return $this->hasOne('App\Aeroporto');
+    }
+
+    public function getAllVoos(){
+        $voos = DB::table('voos as v')
+            ->join('aeronaves as a', 'v.aeronave_id', '=', 'a.id')
+            ->join('aeroportos as ao', 'v.origem_id', '=', 'ao.id') 
+            ->join('aeroportos as ad', 'v.destino_id', '=', 'ad.id')          
+            ->select('v.id','v.numero','v.data','v.hora','a.matricula', 'a.tipo', 'ao.nome as origem', 'ad.nome as destino')
+            ->get();
+            
+        return $voos;
+    }
+
+    public function getById($id){
+        $voo = DB::table('voos as v')
+            ->join('aeronaves as a', 'v.aeronave_id', '=', 'a.id')
+            ->join('aeroportos as ao', 'v.origem_id', '=', 'ao.id') 
+            ->join('aeroportos as ad', 'v.destino_id', '=', 'ad.id') 
+            ->where('v.id', '=', $id)         
+            ->select('v.id','v.numero','v.data','v.hora','a.matricula', 'a.tipo', 'ao.nome as origem', 'ad.nome as destino')            
+            ->get();
+            
+        return $voo;
     }
 
 }
